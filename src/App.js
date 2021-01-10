d472ea500b797d1ada5bff012d298e9df64f0eb7
Matthew <matt-hope@hotmail.com>
2021-01-10 16:55:11 -0500

    added handle submit and handle change functions

--- src/App.js
+++ src/App.js
@@ -9,8 +9,20 @@ import NominationList from './NominationList.js';
   constructor() {
     super();
     this.state = {
+      value: '',
+    };
 
-    }
+    this.handleInputSubmit = this.handleInputSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputSubmit(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
+  handleInputChange(event) {
+    this.setState({value: event.target.value});
   }
 
   componentDidMount() {
@@ -18,16 +30,17 @@ import NominationList from './NominationList.js';
   }
 
   render() {
-    
-    fetch('http://www.omdbapi.com/?s=Star&apikey=82a91ad2')
-    .then(response => response.json())
-    .then(data => console.log(data));
 
     return (
       <div className="App">
         <div className="wrapper">
           <h1>The Shoppies</h1>
-          <SearchForm label="Movie Title" placeholder="Search Movies" id="movie-input" />
+          <SearchForm 
+            label="Movie Title" 
+            placeholder="Search Movies"
+            id="movie-input" 
+            handleSubmit={this.handleInputSubmit} 
+            handleChange={this.handleInputChange}/>
           <ResultList />
           <NominationList />
         </div>
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,11 +1,11 @@
 function Search(props) {
     return (
         <div className="Search">
-            <form> 
+            <form onSubmit={props.handleSubmit}> 
                 <div>
                     <label>{props.label}</label>
-                    <input id={props.id} type="text" placeholder={props.placeholder}/>
-                    <button><i class="fa fa-search"></i></button>
+                    <input id={props.id} type="text" placeholder={props.placeholder} onChange={props.handleChange}/>
+                    <button type="submit"><i className="fa fa-search"></i></button>
                 </div>
             </form>
         </div>
